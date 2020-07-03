0383fcf86d77534c9480ece9d712b161eeedc9a4
Alexander <mrreads@yandex.com>
2020-07-03 16:45:42 +0300

    anim fxOne add

--- js/index.js
+++ js/index.js
@@ -1,34 +1,81 @@
 var controller = new ScrollMagic.Controller();
 
 var imageNumber = 1;
-var walkImages = 
+var images = 
 [
-  "./img/link_walk1.png",
-  "./img/link_walk2.png",
-  "./img/link_walk3.png",
-  "./img/link_walk4.png",
-  "./img/link_walk5.png",
-  "./img/link_walk6.png",
-  "./img/link_walk7.png",
-  "./img/link_walk8.png"
+    "./img/link_walk1.png",
+    "./img/link_walk2.png",
+    "./img/link_walk3.png",
+    "./img/link_walk4.png",
+    "./img/link_walk5.png",
+    "./img/link_walk6.png",
+    "./img/link_walk7.png",
+    "./img/link_walk8.png"
 ];
 
-new ScrollMagic.Scene({
-  duration: 2000,
-  offset: 50,
+var obj = { curImg: 0 };
+
+var tweenWalkAnimation = TweenMax.to(obj, 0.5,
+    {
+        curImg: images.length - 1,
+        roundProps: "curImg",
+        repeat: 3,									
+        immediateRender: true,			
+        ease: Linear.easeNone,			
+        onUpdate: function () {
+            document.querySelector(".link").style.backgroundImage = `url('${images[obj.curImg]}')`;
+        }
+    }
+)
+
+new ScrollMagic.Scene(
+{
+    duration: 2000,
+    offset: 50,
 })
 .setPin(".link")
-.addTo(controller)
-.on("progress", function (e) 
-{
-  if (Math.round(e.progress * 10) < 7)
-  {
-    imageNumber = Math.round(e.progress * 10) + 1;
-  }
-  else
-  {
-    imageNumber = Math.round(e.progress * 10) - 3;
-  }
+.setTween(tweenWalkAnimation)
+.addTo(controller);
 
-  document.querySelector('.link').style.backgroundImage = `url('${walkImages[imageNumber]}')`;
-});
+
+var fxOnePath = 
+{
+    entry : {
+        curviness: 1.25,
+        autoRotate: true,
+        values: [
+                {x: 100,	y: -20},
+                {x: 300,	y: 10}
+            ]
+    },
+    looping : {
+        curviness: 1.25,
+        autoRotate: true,
+        values: [
+                {x: 510,	y: 60},
+                {x: 620,	y: -60},
+                {x: 500,	y: -100},
+                {x: 380,	y: 20},
+                {x: 500,	y: 60},
+                {x: 880,	y: 20},
+                {x: 1320,	y: 15}
+            ]
+    },
+    leave : {
+        curviness: 1.25,
+        autoRotate: true,
+        values: [
+                {x: 660,	y: 20},
+                {x: 800,	y: 130},
+                {x: 1500,	y: -300},
+            ]
+    }
+};
+
+var tween = new TimelineMax()
+.add(TweenMax.to(document.querySelector(".fxOne"), 12.2, {css:{bezier:fxOnePath.looping}, ease:Power1.easeInOut}));
+
+
+new ScrollMagic.Scene({triggerElement: "#trigger", duration: 800, offset: 200})
+.setTween(tween)
+.addTo(controller);
